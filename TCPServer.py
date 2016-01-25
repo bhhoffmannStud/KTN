e57dcc724e4e5fc617043b1167fdee78b5d140dc
bhh <bjohhoffmann@gmail.com>
2016-01-25 20:04:47 +0100

    Fixed some errors in TCP application

--- TCPServer.py
+++ TCPServer.py
@@ -1,15 +1,15 @@
 
-from scoket import *
+from socket import *
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM) #create a welcoming socket
 serverSocket.bind(('',serverPort)) #bind the socket to the port
 serverSocket.listen(1) #argument is max nr of queued connections
-print 'The server is ready to recieve'
+print ('The server is ready to recieve')
 while 1:
     connectionSocket, addr = serverSocket.accept() #connection socket established
     #dedicated to the client requesting connection.
     #Handshake happens between clientSocket and connectionSocket
     sentence = connectionSocket.recv(1024)
-    print sentence
+    print (sentence)
     connectionSocket.close()
